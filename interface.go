bbdc45bcf55de61b38b4108871199a117aecd1be
Brett Lempereur <b.lempereur@outlook.com>
2016-12-14 15:04:57 +0000

    Fixed PerceptualHash interface definition

--- interface.go
+++ interface.go
@@ -7,7 +7,7 @@ import "image"
 // PerceptualHash is the interface implemented by perceptual hash algorithms.
 type PerceptualHash interface {
 	// Hash computes the perceptual hash of an image.
-	Hash(img *image.Image) ([]byte, error)
+	Hash(img image.Image) ([]byte, error)
 	// Length returns the length of hashes computed by this percepual hash
 	// in bytes.
 	Length() int
