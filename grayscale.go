6b65430d6f0cd4346246bf6998f4517fb4ac8be8
Brett Lempereur <b.lempereur@outlook.com>
2016-12-14 12:53:38 +0000

    Added lookup tables for grayscale conversion

--- grayscale.go
+++ grayscale.go
@@ -2,6 +2,12 @@ package ish
 
 import "image"
 
+// Lookup tables using BT.601 constants for colour channel to grayscale
+// conversion.
+var redToGray = grayscaleTable(0.2989)
+var greenToGray = grayscaleTable(0.5870)
+var blueToGray = grayscaleTable(0.1140)
+
 // Grayscale converts an image to the grayscale colour space, using optimised
 // algorithms for common source colour spaces.
 func Grayscale(img image.Image) *image.Gray {
@@ -33,11 +39,11 @@ func grayscaleRGBA(img *image.RGBA) *image.Gray {
 	gray := image.NewGray(img.Bounds())
 	p := 0
 	for q := 0; q < len(img.Pix); q += 4 {
-		r := float32(img.Pix[q])
-		g := float32(img.Pix[q+1])
-		b := float32(img.Pix[q+2])
-		// Grayscale conversion using BT.601 constants.
-		gray.Pix[p] = uint8(0.2989*r + 0.5870*g + 0.1140*b)
+		r := img.Pix[q]
+		g := img.Pix[q+1]
+		b := img.Pix[q+2]
+		// Grayscale conversion using BT.601 lookup tables.
+		gray.Pix[p] = uint8(redToGray[r] + greenToGray[g] + blueToGray[b])
 		p++
 	}
 	return gray
@@ -51,12 +57,22 @@ func grayscaleNRGBA(img *image.NRGBA) *image.Gray {
 	for q := 0; q < len(img.Pix); q += 4 {
 		// Alpha premultiplication.
 		alpha := float32(img.Pix[q+3]) / 255.0
-		r := float32(img.Pix[q]) * alpha
-		g := float32(img.Pix[q+1]) * alpha
-		b := float32(img.Pix[q+2]) * alpha
-		// Grayscale conversion using BT.601 constants.
-		gray.Pix[p] = uint8(0.2989*r + 0.5870*g + 0.1140*b)
+		r := uint8(float32(img.Pix[q]) * alpha)
+		g := uint8(float32(img.Pix[q+1]) * alpha)
+		b := uint8(float32(img.Pix[q+2]) * alpha)
+		// Grayscale conversion using BT.601 lookup tables.
+		gray.Pix[p] = uint8(redToGray[r] + greenToGray[g] + blueToGray[b])
 		p++
 	}
 	return gray
+}
+
+// grayscaleTable implements an algorithm to generate lookup tables for colour
+// to grayscale conversion.
+func grayscaleTable(factor float64) [256]uint8 {
+	var output [256]uint8
+	for i := 0; i < 256; i++ {
+		output[i] = uint8(float64(i) * factor)
+	}
+	return output
 }
